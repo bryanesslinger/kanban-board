d38f662bf76d583c69216f11dd72eb253ee52200
bryanesslinger <bryanessl@gmail.com>
2025-02-06 18:40:56 -0500

    updated connection files for render deploy

--- Develop/server/dist/models/index.js
+++ Develop/server/dist/models/index.js
@@ -3,15 +3,35 @@ dotenv.config();
 import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
-const sequelize = process.env.DB_URL
-    ? new Sequelize(process.env.DB_URL)
-    : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+// const sequelize = process.env.DB_URL
+//   ? new Sequelize(process.env.DB_URL)
+//   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+//       host: 'localhost',
+//       dialect: 'postgres',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+let sequelize;
+if (process.env.DB_URL) {
+    sequelize = new Sequelize(process.env.DB_URL);
+}
+else {
+    sequelize = new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
         host: 'localhost',
         dialect: 'postgres',
         dialectOptions: {
             decimalNumbers: true,
+            // sequelize = new Sequelize(
+            //   process.env.DB_NAME,
+            //   process.env.DB_USER,
+            //   process.env.DB_PW,
+            //   {
+            //     host: 'localhost',
+            //     dialect: 'postgres',
         },
     });
+}
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
 User.hasMany(Ticket, { foreignKey: 'assignedUserId' });
